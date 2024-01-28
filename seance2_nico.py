c2404ac872c07388443913b455fdfd0c6a852257
Charpy <nico_github@charpenel.org>
2024-01-28 22:26:51 +0100

    Mouvement relatif à la direction du regard, vitesse de marche/course et corrections de bugs.

--- seance2_nico.py
+++ seance2_nico.py
@@ -4,26 +4,28 @@
 # module pyglet principal (qu'on nenomme en pg pour plus de simplicité)
 import pyglet as pg
 from pyglet import shapes
-from math import cos, sin
+from math import cos, sin, pi
 
 # Configuration des commandes
-cmd = {"up":    ["pg.window.key.UP","pg.window.key.Z",122],
-       "down":  ["pg.window.key.DOWN","pg.window.key.S"],
-       "left":  ["pg.window.key.LEFT","pg.window.key.Q"],
-       "right": ["pg.window.key.RIGHT","pg.window.key.D"],
-       "reload":["pg.window.key.R", 65456],  #Touche 0 du pad num
-       "walk":  [65505 , 65506], #Touche MAJG ou MAJD
-       "jump":  [32 , 65508], #SPACE ou CTRLD
-       "fire":  ["pyglet.window.mouse.LEFT"]
+cmd = {"forward":    [pg.window.key.UP , pg.window.key.Z], #vers l'avant : Z ou haut
+       "backward":  [pg.window.key.DOWN , pg.window.key.S], #vers l'arrière : S ou bas
+       "straf_left":  [pg.window.key.LEFT , pg.window.key.Q], #déplacement latéral gauche : Q ou gauche
+       "straf_right": [pg.window.key.RIGHT , pg.window.key.D], #déplacement latéral droite : D ou droite
+       "reload":[pg.window.key.R, 65456],  #recharger : R ou Touche 0 du pad num
+       "use":[pg.window.key.E, 65457], # utiliser : E ou Touche 1 du pad num
+       "walk":  [65505 , 65506], #Marcher : Touche MAJG ou MAJD
+       "jump":  [32 , 65508], #Sauter : SPACE ou CTRLD
+       "fire":  [pg.window.mouse.LEFT]
        }
 
-global x_joueur, y_joueur, angle_joueur_horizontal, sensi_horizontale
+global x_joueur, y_joueur, angle_joueur, sensi_horizontale, vitesse
 fenetre2D_largeur = 640
 fenetre2D_hauteur = 400
 x_joueur = fenetre2D_largeur // 2
 y_joueur = fenetre2D_hauteur // 2
-angle_joueur_horizontal = 0
-sensi_horizontale = 0.02
+angle_joueur = 0 # Angle de déplacement
+sensi_horizontale = 0.01
+vitesse = 25
 
 # création de la fenêtre pour le plan 2D
 # résolution : 320x200 (comme le Doom de l'époque)
@@ -35,32 +37,59 @@ joueur = pg.graphics.Batch()
 # détection d'un mouvement de la souris
 @window2d.event
 def on_mouse_motion(x, y, dx, dy):
-    global x_joueur, y_joueur, angle_joueur_horizontal
+    global x_joueur, y_joueur, angle_joueur
     print("Souris x, y, dx, dy :", "\t", x, "\t", y, "\t", dx, "\t", dy)
-    angle_joueur_horizontal += sensi_horizontale*dx
+    angle_joueur += sensi_horizontale*dx
+    print("Angle joueur : ", angle_joueur)
+    
+    angle_joueur = angle_joueur % (2*pi) # angle compris entre 0 et 2pi
 
 # détection d'un touche pressée au clavier
 @window2d.event
 def on_key_press(symbol, modifiers):
-  global x_joueur, y_joueur, angle_joueur_horizontal
+  global x_joueur, y_joueur, angle_joueur, vitesse
 
   print("Touche pressée n°", symbol)
 
-  if symbol == 65307: pg.app.exit() # Echap
+  if symbol == 65307: pg.app.exit() # Echap pour quitter
   
-#  if symbol == pg.window.key.Z or symbol == pg.window.key.UP: #haut
-  if symbol in cmd["up"]: #haut
-      y_joueur += 10
+  if symbol in cmd["forward"]: #haut
+      x_joueur += vitesse * cos(angle_joueur)
+      y_joueur += vitesse * sin(angle_joueur)
 
-  if symbol == pg.window.key.S or symbol == pg.window.key.DOWN: #bas
-      y_joueur -= 10
+  if symbol in cmd["backward"]: #bas
+      x_joueur -= vitesse * cos(angle_joueur)
+      y_joueur -= vitesse * sin(angle_joueur)
 
-  if symbol == pg.window.key.Q or symbol == pg.window.key.LEFT: #gauche
-      x_joueur -= 10
+  if symbol in cmd["straf_left"]: #gauche
+      x_joueur -= vitesse * sin(angle_joueur)
+      y_joueur += vitesse * cos(angle_joueur)
 
-  if symbol == pg.window.key.D or symbol == pg.window.key.RIGHT: #droite
-      x_joueur += 10
+  if symbol in cmd["straf_right"]: #droite
+      x_joueur += vitesse * sin(angle_joueur)
+      y_joueur -= vitesse * cos(angle_joueur)
 
+  if symbol in cmd["reload"]: #recharger
+      print("Rechargement arme")
+
+  if symbol in cmd["use"]: #marcher au lieu de courir
+      # Diminuer vitesse d'avance du joueur
+      print("Utiliser")
+
+  if symbol in cmd["walk"]: #marcher au lieu de courir
+      # Diminuer vitesse d'avance du joueur
+      vitesse /= 2.5
+      print("Marcher")
+
+  if symbol in cmd["jump"]: #saut
+      #Sauter !
+      print("Sauter")
+      pass
+
+  if symbol in cmd["fire"]: #Tir principal
+      #Tirer
+      print("Tir")
+      pass
 '''
   if symbol == pg.window.key.UP or symbol == pg.window.key.LEFT: #fleche haut
        angle_joueur_horizontal += 0.5
@@ -69,17 +98,23 @@ def on_key_press(symbol, modifiers):
        angle_joueur_horizontal -= 0.5
 '''
        
-# détection d'un touche relâchée au clavier
+# détection d'une touche relâchée au clavier
 @window2d.event
 def on_key_release(symbol, modifiers):
+    global vitesse
+
     print("Touche relâchée n°", symbol)
+
+    if symbol in cmd["walk"]: #Cesser de marcher et re-courir
+      vitesse *= 2.5
+      print("Courir")
 
 # évènement principal : rendu graphique
 @window2d.event
 def on_draw():
     window2d.clear()
     cercle = shapes.Circle(x_joueur, y_joueur, radius=20, color=(50, 225, 30), batch = joueur)
-    line = shapes.Line(x_joueur, y_joueur, x_joueur + 100*cos(angle_joueur_horizontal), y_joueur+100*sin(angle_joueur_horizontal), width=8, batch=joueur)
+    line = shapes.Line(x_joueur, y_joueur, x_joueur + 100*cos(angle_joueur), y_joueur+100*sin(angle_joueur), width=7, batch=joueur)
     joueur.draw()
 
 # lancement du jeu
